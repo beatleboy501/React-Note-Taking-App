7202710ab9826b9fa5546f9cb62724dac0e47349
Andrew Allison <beatleboy501@gmail.com>
2016-04-14 03:52:59 -0400

    Update NotesList.jsx

--- app/client/views/NotesList.jsx
+++ app/client/views/NotesList.jsx
@@ -12,36 +12,36 @@ NotesList = React.createClass({
     }
   },
   handleInsertNote(inputTitle, inputValue) {
-	   Meteor.call('/note/insert', inputTitle, inputValue, function(err, result){
-	   	 if (err) { console.log('there was an error: ' + err.reason); };
+      Meteor.call('/note/insert', inputTitle, inputValue, function(err, result){
+   	 if (err) { console.log('there was an error: ' + err.reason); };
     });
   },
-	handleUpdateNote(inputTitle, inputValue, id) {
-		Meteor.call('/note/update', { title: inputTitle, text: inputValue, id: id}, function(err, result){
-			if(err) { console.log('there was an error: ' + err.reason); };
-		});
-	},
-	handleDeleteNote(note) {
-		let deleteConfirm = confirm("Are you sure you wish to delete '" + note.title + "'?");
-		if(deleteConfirm) {
-			Meteor.call('/note/delete', note._id, function(err, result) {
-				if(err) {
-					console.log('there was an error: ' + err.reason);
-        };
-			});
-			sAlert.info("Your note has been deleted", {effect: 'stackslide', position: 'bottom-right', timeout: 2000,});
-		}
-	},
+  handleUpdateNote(inputTitle, inputValue, id) {
+      Meteor.call('/note/update', { title: inputTitle, text: inputValue, id: id}, function(err, result){
+	if(err) { console.log('there was an error: ' + err.reason); };
+      });
+  },
+  handleDeleteNote(note) {
+      let deleteConfirm = confirm("Are you sure you wish to delete '" + note.title + "'?");
+      if(deleteConfirm) {
+      	Meteor.call('/note/delete', note._id, function(err, result) {
+      	  if(err) {
+      	    console.log('there was an error: ' + err.reason);
+          };
+        });
+	sAlert.info("Your note has been deleted", {effect: 'stackslide', position: 'bottom-right', timeout: 2000,});
+      }
+  },
   showNotes(){
    return this.data.subsReady?
        <List
            collection={this.data.notes}
            handleAddItem={this.handleInsertNote}
-					 handleDeleteItem={this.handleDeleteNote}
-					 handleEditItem={this.handleUpdateNote}
+	   handleDeleteItem={this.handleDeleteNote}
+	   handleEditItem={this.handleUpdateNote}
            canAddItemItem={true}
-					 canDeleteItem={true}
-					 canEditItem={true}
+	   canDeleteItem={true}
+	   canEditItem={true}
            newItemPlaceHolder="New Note..."
        />
        :
@@ -52,19 +52,19 @@ NotesList = React.createClass({
   },
   render() {
       return (
-          <div className="row">
-              <div className="col-md-6 col-md-offset-3">
-								<PageTitle title="Create Note" />
-									<ul className="list-group">
-										<li className="list-group-item editable">
-											<SingleNoteSubmit
-												placeholder="New note..."
-												handleInput={this.handleInsertNote}
-											/>
-										</li>
-									</ul>
-                  {this.showNotes()}
-              </div>
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3">
+	    <PageTitle title="Create Note" />
+	      <ul className="list-group">
+	        <li className="list-group-item editable">
+	          <SingleNoteSubmit
+	            placeholder="New note..."
+	            handleInput={this.handleInsertNote}
+	          />
+	        </li>
+	      </ul>
+	      {this.showNotes()}
+	    </div>
           </div>
       );
   }
